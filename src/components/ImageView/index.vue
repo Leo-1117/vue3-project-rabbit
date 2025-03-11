3170bb38f4f43f1376c00068409645d99e79d8a8
Leo <2376611270@qq.com>
2025-03-11 15:13:07 +0800

    图片预览组件-props适配和优化操作

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -2,14 +2,13 @@
 import { ref, watch } from "vue";
 import { useMouseInElement } from "@vueuse/core";
 
-// 图片列表
-const imageList = [
-  "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
-  "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
-  "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
-  "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
-  "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg",
-];
+//props适配picture list
+defineProps({
+  imageList: {
+    type: Array,
+    default: () => [],
+  },
+});
 
 //小图切换大图显示
 const activeIndex = ref(0);
@@ -29,7 +28,9 @@ const top = ref(0);
 const positionX = ref(0);
 const positionY = ref(0);
 watch([elementX, elementY, isOutside], () => {
-  if (isOutside) return;
+  if (!isOutside) {
+    return;
+  }
   //有效范围内控制滑块距离
   //横向
   if (elementX.value > 100 && elementX.value < 300) {
