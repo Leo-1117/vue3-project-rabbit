26cb30f77ff4c52f45b9efbf16adc759d8ad0677
Leo <2376611270@qq.com>
2025-03-12 08:07:01 +0800

    本地购物车-添加购物车实现

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,7 +3,9 @@ import { getDetail } from "@/apis/detail";
 import { useRoute } from "vue-router";
 import { ref, onMounted } from "vue";
 import DetailHot from "./components/DetailHot.vue";
-
+import { ElMessage } from "element-plus";
+import { useCartStore } from "@/stores/cartStore";
+const cartStore = useCartStore();
 const goods = ref({});
 const route = useRoute();
 const getGoods = async () => {
@@ -13,8 +15,35 @@ const getGoods = async () => {
 onMounted(() => getGoods());
 
 //sku规格被操作时
+let skuObj = {};
 const skuChange = (sku) => {
   console.log("SKU被操作");
+  skuObj = sku;
+};
+
+//count
+const count = ref(1);
+const countChange = (count) => {
+  console.log(count);
+};
+
+//add card
+const addCart = () => {
+  if (skuObj.skuId) {
+    //规则已经选择 触发action
+    cartStore.addCart({
+      id: goods.value.id,
+      name: goods.value.name,
+      pictures: goods.value.pictures,
+      count: count.value,
+      skuId: skuObj.skuId,
+      attrsText: skuObj.specsText,
+      seleted: true,
+    });
+  } else {
+    //规则未选择 提示用户
+    ElMessage.warning("请选择规格");
+  }
 };
 </script>
 
@@ -95,10 +124,10 @@ const skuChange = (sku) => {
               <!-- sku组件 -->
               <LeoSku :goods="goods" @change="skuChange" />
               <!-- 数据组件 -->
-
+              <el-input-number v-model="count" @change="countChange" />
               <!-- 按钮组件 -->
               <div>
-                <el-button size="large" class="btn"> 加入购物车 </el-button>
+                <el-button size="large" class="btn" @click="addCart"> 加入购物车 </el-button>
               </div>
             </div>
           </div>
